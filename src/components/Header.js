8495b139958de29bedf9c6205a3bbd742f58c2df
piotrek <piotr.psujek@gmail.com>
2017-11-18 10:45:44 +0100

    Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Menu from './Menu';
+import logo from'../logo.svg';
 
 const Header = () => {
     return (
         <div>
             <h1>Header</h1>
+            <img src={logo}/>
             <Menu/>
         </div>
     );
