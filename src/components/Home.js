751055dbde27517fc71f1b9d110c50e2825d8eda
piotrek <piotr.psujek@gmail.com>
2017-11-19 09:56:21 +0100

    formularze react

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import {Grid, Row, Col, Button} from 'react-bootstrap';
+
+import {Grid, Row, Col, Button, FormGroup, ControlLabel, FormControl, HelpBlock, Checkbox} from 'react-bootstrap';
 
 const Home = () => {
     const handleClick = () => {
@@ -15,8 +16,25 @@ const Home = () => {
                     <Col xs={12} md={8}> Column 1</Col>
                     <Col xs={12} md={4}> Column 2</Col>
                 </Row>
-
             </Grid>
+
+            <form>
+                <FormGroup>
+                    <ControlLabel>Label</ControlLabel>
+                    <FormControl type="text" placeholder="Enter text"/>
+                    <HelpBlock>Hel Block</HelpBlock>
+                </FormGroup>
+                    <Checkbox inline>
+                        1
+                    </Checkbox>
+                <FormGroup>
+                    <FormControl componentClass='select' placeholder="select">
+                            <option value="select">select</option>
+                            <option value="other">...</option>
+                        </FormControl>
+                    </FormGroup>
+
+            </form>
 
 
         </div>
