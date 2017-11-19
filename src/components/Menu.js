f0f81bd555307464be163a09c8734b5698e322aa
piotrek <piotr.psujek@gmail.com>
2017-11-19 11:51:30 +0100

    Routing cd

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import {Nav, NavItem} from 'react-bootstrap'
-import {Link} from 'react-router-dom';
+
+import {LinkContainer} from 'react-router-bootstrap';
 
 const Menu = () => {
     return (
         <div>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/dashboard">Dashboard</Link></li>
+        {/*<ul>*/}
+            {/*<li><Link to="/">Home</Link></li>*/}
+            {/*<li><Link to="/profile">Profile</Link></li>*/}
+            {/*<li><Link to="/dashboard">Dashboard</Link></li>*/}
 
-        </ul>
+        {/*</ul>*/}
 
 
-            {/*<Nav bsStyle="pills" activeKey={0} >*/}
-                {/*<NavItem eventKey={0}>Home</NavItem>*/}
-                {/*<NavItem eventKey={1}>Profile</NavItem>*/}
-                {/*<NavItem eventKey={2}>Dashboard</NavItem>*/}
-            {/*</Nav>*/}
-
+            <Nav bsStyle="pills" >
+                <LinkContainer exact to="/"><NavItem>Home</NavItem></LinkContainer>
+                <LinkContainer to="/profile"><NavItem>Profile</NavItem></LinkContainer>
+                <LinkContainer to="/dashboard"><NavItem>Dashboard</NavItem></LinkContainer>
+            </Nav>
         </div>
     );
 };
