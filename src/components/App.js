4fecd1d961e4b29f687e7ac95f4101e58c17ecdc
piotrek <piotr.psujek@gmail.com>
2017-11-19 10:28:23 +0100

    Router

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,30 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
+import Profile from './Profile';
+import Dashboard from './Dashboard';
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
+
 const App = () => {
     return (
-        <div>
+
+        <Router>
+            <div>
             <Header/>
-            <h1>Hello World!</h1>
-            <Home/>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/">Profile</Link></li>
+                    <li><Link to="/">Dashboard</Link></li>
+                </ul>
+
+                <Route exact path="/" component={Home}/>
+                <Route path="/profile" component={Profile}/>
+                <Route path="/dashboard" component={Dashboard}/>
+
             <Footer/>
-        </div>
+            </div>
+        </Router>
+
     );
 };
 
